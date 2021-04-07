134f8471e8487c25d4e3e3012d5b9a322919e4f7
avigator003 <akshatlakshkar.3@gmail.com>
2021-04-08 02:31:31 +0530

    OMR Confidence Percentage Done

--- src/Components/Omr.js
+++ src/Components/Omr.js
@@ -3,7 +3,7 @@ import Avatar from 'react-avatar'
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { green, red, orange } from "@material-ui/core/colors"
 import Grid from "@material-ui/core/Grid";
-import { Radio } from "@material-ui/core";
+import { Radio ,RadioGroup} from "@material-ui/core";
 
 
 const GreenRadio = withStyles({
@@ -38,18 +38,32 @@ const RedRadio = withStyles({
     checked: {},
 })((props) => <Radio color="default" {...props} />);
 
+
+
+
 function Omr() {
     const classes = useStyles();
 
     const [answerArray, setAnswerArray] = useState([])
+    const[percentageArray,setPercentageArray]=useState([])
 
-    const handleOmr = (option, index) => {
-        console.log(index, option)
+    const handleOmrAnswer = (option,index) => {
+        console.log(option,index)
         let array = [...answerArray]
         array[index] = option
         console.log(array)
         setAnswerArray(array)
     }
+
+    
+    const handleOmrAnswerPercentage = (option,index) => {
+        let array = [...percentageArray]
+        array[index] = option
+        console.log(array)
+        setPercentageArray(array)
+    }
+
+
     return (
         <div className={classes.root}>
 
@@ -62,94 +76,102 @@ function Omr() {
                     [...Array(3),].map((value, index) => (
                         <>
 
-                        <Grid item xs={12} sm={12} md={3} lg={3} key={index} className={classes.columnContainer}>
-                        <div style={{borderBottom:"1px solid black",display:"flex",flexDirection:"row"}}>
-                 
-                 
-                         <div >go</div>
-                         <div style={{borderLeft:"1px solid black",height:50}}>
-                         <div className={classes.feedbackHeading}>
-                             <div>hey</div>
-                             <div>hey</div>
-                             <div>hey</div>
-                                          
-                         </div>
+                            <Grid item xs={12} sm={12} md={3} lg={3} key={index} className={classes.columnContainer}>
 
-                         </div>
+                                <div className={classes.feedbackContainer}>
+                                    <div>Confidence Percentage</div>
+                                    <div className={classes.feedbackHeading}>
+                                        <div className={classes.confidenceLevel}>10%</div>
+                                        <div className={classes.confidenceLevel}>50%</div>
+                                        <div className={classes.confidenceLevel}>100%</div>
 
-                        </div>
-
-                            {
-                                [...Array(35),].map((value, i) => (
-                                    <div className={classes.avatarContainer} key={i}>
-                                        <div className={classes.index}>
-                                            <p className={classes.indexText}>
-                                                {(35 * index) + (i + 1)}
-                                            </p>
-                                        </div>
-                                        <div className={classes.avatar}>
-                                            <Avatar size="15" round={true} name={"A"} color={answerArray[(35 * index) + (i + 1)] === "A" ? "black" : "transparent"}
-                                                fgColor={answerArray[(35 * index) + (i + 1)] === "A" ? "white" : "#DF46DE"} className={classes.avatarLogo}
-                                                onClick={() => handleOmr("A", (35 * index) + (i + 1))} textSizeRatio={2} />
-                                        </div>
-                                        <div className={classes.avatar}>
-
-                                            <Avatar size="15" round={true} name={"B"} color={answerArray[(35 * index) + (i + 1)] === "B" ? "black" : "transparent"}
-                                                fgColor={answerArray[(35 * index) + (i + 1)] === "B" ? "white" : "#DF46DE"} className={classes.avatarLogo}
-                                                onClick={() => handleOmr("B", (35 * index) + (i + 1))} textSizeRatio={2} />
-                                        </div>
-
-                                        <div className={classes.avatar}>
-                                            <Avatar size="15" round={true} name={"C"} color={answerArray[(35 * index) + (i + 1)] === "C" ? "black" : "transparent"}
-                                                fgColor={answerArray[(35 * index) + (i + 1)] === "C" ? "white" : "#DF46DE"} className={classes.avatarLogo}
-                                                onClick={() => handleOmr("C", (35 * index) + (i + 1))} textSizeRatio={2} />
-                                        </div>
-
-                                        <div className={classes.avatar}>
-                                            <Avatar size="15" round={true} name={"D"} color={answerArray[(35 * index) + (i + 1)] === "D" ? "black" : "transparent"}
-                                                fgColor={answerArray[(35 * index) + (i + 1)] === "D" ? "white" : "#DF46DE"} className={classes.avatarLogo}
-                                                onClick={() => handleOmr("D", (35 * index) + (i + 1))} textSizeRatio={2} />
-                                        </div>
-
-
-                                        <div className={classes.feedback}>
-                                            <RedRadio
-                                                size="small"
-                                                value="c"
-                                                name="radio-button-demo"
-                                                inputProps={{ 'aria-label': 'C' }}
-                                                label="10%"
-                                                labelPlacement="bottom"
-                                            />
-
-                                            <OrangeRadio
-                                                size="small"
-                                                value="c"
-                                                name="radio-button-demo"
-                                                label="50%"
-                                                labelPlacement="bottom"
-                                                inputProps={{ 'aria-label': 'C' }}
-
-                                            />
-
-                                            <GreenRadio
-                                                size="small"
-                                                value="c"
-                                                name="radio-button-demo"
-                                                inputProps={{ 'aria-label': 'C' }}
-                                                label="100%"
-                                                labelPlacement="bottom"
-                                            
-                                            />
-                                        </div>
 
                                     </div>
+                                </div>
 
-                                ))
+                                {
+                                    [...Array(35),].map((value, i) => (
+                                        <div className={classes.avatarContainer} key={i}>
+                                            <div className={classes.index}>
+                                                <p className={classes.indexText}>
+                                                    {(35 * index) + (i + 1)}
+                                                </p>
+                                            </div>
+                                            <div className={classes.avatar}>
+                                                <Avatar size="15" round={true} name={"A"} color={answerArray[(35 * index) + i ]=== "A" ? "black" : "transparent"}
+                                                    fgColor={answerArray[(35 * index) + i ] === "A" ? "white" : "#DF46DE"} className={classes.avatarLogo}
+                                                    onClick={() => handleOmrAnswer("A", (35 * index) + i )} textSizeRatio={2} />
+                                            </div>
+                                            <div className={classes.avatar}>
 
-                            }
-                        </Grid>
-</>
+                                                <Avatar size="15" round={true} name={"B"} color={answerArray[(35 * index) + i] === "B" ? "black" : "transparent"}
+                                                    fgColor={answerArray[(35 * index) + i] === "B" ? "white" : "#DF46DE"} className={classes.avatarLogo}
+                                                    onClick={() => handleOmrAnswer("B", (35 * index) + i )} textSizeRatio={2} />
+                                            </div>
+
+                                            <div className={classes.avatar}>
+                                                <Avatar size="15" round={true} name={"C"} color={answerArray[(35 * index) + i]=== "C" ? "black" : "transparent"}
+                                                    fgColor={answerArray[(35 * index) + i ] === "C" ? "white" : "#DF46DE"} className={classes.avatarLogo}
+                                                    onClick={() => handleOmrAnswer("C", (35 * index) + i )} textSizeRatio={2} />
+                                            </div>
+
+                                            <div className={classes.avatar}>
+                                                <Avatar size="15" round={true} name={"D"} color={answerArray[(35 * index) + i ] === "D" ? "black" : "transparent"}
+                                                    fgColor={answerArray[(35 * index) + i ] === "D" ? "white" : "#DF46DE"} className={classes.avatarLogo}
+                                                    onClick={() => handleOmrAnswer("D", (35 * index) + i )} textSizeRatio={2} />
+                                            </div>
+
+
+
+                                            <RadioGroup
+                                                aria-label="position"
+                                                name="position"
+                                                value={answerArray[(35 * index) + i]?.percentage}
+                                                onChange={(e)=>handleOmrAnswerPercentage(e.target.value, ((35 * index) + i ))}
+                                                className={classes.feedback}
+                                                row
+                                            >
+                                                <RedRadio
+                                                    className={classes.radio}
+                                                    size="small"
+                                                    value="10%"
+                                                    name="radio-button-demo"
+                                                    inputProps={{ 'aria-label': 'C' }}
+                                                    label="10%"
+                                                    labelPlacement="bottom"
+                                                />
+
+                                                <OrangeRadio
+                                                    className={classes.radio}
+                                                    size="small"
+                                                    value="50%"
+                                                    name="radio-button-demo"
+                                                    label="50%"
+                                                    labelPlacement="bottom"
+                                                    inputProps={{ 'aria-label': 'C' }}
+
+                                                />
+
+                                                <GreenRadio
+                                                    className={classes.radio}
+                                                    size="small"
+                                                    value="100%"
+                                                    name="radio-button-demo"
+                                                    inputProps={{ 'aria-label': 'C' }}
+                                                    label="100%"
+                                                    labelPlacement="bottom"
+
+                                                />
+
+                                            </RadioGroup>
+                                        </div>
+
+
+                                    ))
+
+                                }
+                            </Grid>
+                        </>
                     ))}
 
                 <Grid item xs={12} sm={12} md={1} lg={1} />
@@ -173,6 +195,8 @@ const useStyles = makeStyles((theme) => ({
         flexDirection: "row",
     },
     columnContainer: {
+        display: "flex",
+        flexDirection: "column",
         border: "1px solid #DF46DE",
         margin: "5px",
         width: "100%",
@@ -225,15 +249,44 @@ const useStyles = makeStyles((theme) => ({
         },
 
     },
-
+    feedbackContainer: {
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px",
+        borderBottom: "1px solid black",
+    },
     feedback: {
         borderLeft: "1px solid #DF46DE",
-        height: "40px"
+        height: "40px",
     },
     feedbackHeading: {
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"flex-end"
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-end",
+        [theme.breakpoints.down('sm')]: {
+            marginRight: 80,
+
+        },
+        [theme.breakpoints.down('xs')]: {
+            marginRight: 0,
+
+        },
+
+    },
+    confidenceLevel: {
+        marginRight: 10,
+        fontSize: 12,
+        [theme.breakpoints.down('sm')]: {
+            marginRight: 10,
+
+        },
+        [theme.breakpoints.down('xs')]: {
+            marginRight: 10,
+
+        }
+    },
+
+    radios: {
     }
 
 
